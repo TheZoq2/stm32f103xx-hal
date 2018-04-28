b40f2ca736088c7e674dece9e8f1ef34989acb3d
TheZoq2 <frans.skarman@gmail.com>
2018-04-28 18:29:24 +0200

    Initial impl of Stopwatch

--- src/mono_timer.rs
+++ src/mono_timer.rs
@@ -6,7 +6,7 @@ use embedded_hal_time::Stopwatch;
 use rcc::{APB1, Clocks};
 use time::Hertz;
 
-struct MonoTimer<TIM> {
+pub struct MonoTimer<TIM> {
     tim: TIM,
     clocks: Clocks
 }
@@ -21,20 +21,45 @@ pub enum Event {
 macro_rules! hal {
     ($($TIMX:ident: ($timX:ident, $timXen:ident, $timXrst:ident),)+) => {
         $(
-            impl MonoTimer<$timX> {
+            impl MonoTimer<$TIMX> {
                 pub fn $timX<T>(tim: $TIMX, clocks: Clocks, apb1: &mut APB1) -> Self {
                     // enable and reset peripheral to a clean slate state
                     apb1.enr().modify(|_, w| w.$timXen().set_bit());
                     apb1.rstr().modify(|_, w| w.$timXrst().set_bit());
                     apb1.rstr().modify(|_, w| w.$timXrst().clear_bit());
 
-                    let result = MonoTimer { clocks, tim };
+                    // Set overflow to max
+                    tim.arr.write(|w| { w.arr().bits(0xffff) });
+
+                    // Set prescaler
+                    tim.psc.write(|w| w.psc().bits(0));
+
+                    // Trigger an update event to load the prescaler value to the clock
+                    tim.egr.write(|w| w.ug().set_bit());
+                    // The above line raises an update event which will indicate
+                    // that the timer is already finnished. Since this is not the case,
+                    // it should be cleared
+                    tim.sr.modify(|_, w| w.uif().clear_bit());
+
+                    tim.cr1.modify(|_r, w| {
+                        // Set clock division to 1, direction to up and enable
+                        w
+                            .ckd().no_div()
+                            .dir().up()
+                            .cen().set_bit()
+                    });
+
+                    MonoTimer { clocks, tim }
                 }
             }
 
-            impl Stopwatch<u16, Hertz> for MonoTimer<$timX> {
+            impl Stopwatch<u16, Hertz> for MonoTimer<$TIMX> {
                 fn ticks_passed(&self) -> u16 {
-                    self.tim.arr.read()
+                    self.tim.cnt.read().cnt().bits()
+                }
+
+                fn frequency(&self) -> Hertz {
+                    self.clocks.pclk1()
                 }
             }
         )+
